229abc48f0a7c6c746cf35db2c764aaa64a2843c
a7384 <51829151+a7384@users.noreply.github.com>
2019-07-03 21:47:04 +0530

    Add files via upload

---
+++ resources/js/min.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+//typed
+    var typed = new Typed('.typed',{
+        strings:['Web Designer','Developer', 'Freelancer' , 'Wordpress Exppert'],
+        typeSpeed:20,
+        backSpeed:15,
+        startDelay:1000,
+        backDelay:1000,
+        loop:true,
+    });
+    //progress
+    $('#bar1').barfiller({barColor:"#E91E63",duration:5000});
+    $('#bar2').barfiller({barColor:"#E91E63",duration:5000});
+    $('#bar3').barfiller({barColor:"#E91E63",duration:5000});
+    $('#bar4').barfiller({barColor:"#E91E63",duration:5000});
+    $('#bar5').barfiller({barColor:"#E91E63",duration:5000});
+    $('#bar6').barfiller({barColor:"#E91E63",duration:5000});
+    
+    
+});
